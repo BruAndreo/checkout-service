cc1bd8f13e291bd7eeeb206baf878394d61258bb
Bruno Andreo <brunolino2026@gmail.com>
2022-03-11 08:52:39 -0300

    feat: Added validation of quantity

--- src/domain/checkout.ts
+++ src/domain/checkout.ts
@@ -1,3 +1,4 @@
+import { ValidationException } from "../helpers/exceptions";
 import Product from "./product";
 
 type ProductBase = {
@@ -14,7 +15,13 @@ export default class Checkout {
   }
 
   private validateProducts(products: Array<ProductBase>) {
-    this.products = products.map(product => new Product(product.id, product.quantity));
+    this.products = products.map(product => {
+      if (product.quantity < 1) {
+        throw new ValidationException("Quantity is not ", 400);
+      }
+
+      return new Product(product.id, product.quantity);
+    });
   }
 
 }
---
+++ tests/unit/domain/checkout.test.ts
@@ -0,0 +1,19 @@
+import Checkout from "../../../src/domain/checkout";
+import { ValidationException } from "../../../src/helpers/exceptions";
+
+
+describe("Checkout Doamin", () => {
+
+  test("Should throw exception when quantity of item is less than 1", () => {
+    const t = () => new Checkout([{ id: 1, quantity: 0 }]);
+
+    expect(t).toThrow(ValidationException);
+  });
+
+  test("Should validate and load Prodcts when the id and quantity are ok", () => {
+    const checkout = new Checkout([{ id: 1, quantity: 1 }]);
+
+    expect(checkout).not.toBeNull();
+  });
+
+});
